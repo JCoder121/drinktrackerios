00cf92b0c4d9957cef443a8cd5a7ca91b715de34
JCoder121 <j.chen14641@gmail.com>
2019-07-09 17:09:02 -0700

    Update drinktrackerv1.py

--- drinktrackerv1.py
+++ drinktrackerv1.py
@@ -1,11 +1,11 @@
 #drinktracker program on iphone, optimized
 #for vertical looking
 #v1.1 published onto github
+#v1.2 working on getting everything into the middle, editing button/label frames
 
 import ui
 import console
 
-testvar = 0
 total = 0
 
 def button_tapped(sender):
@@ -21,25 +21,23 @@ def reset_tap(sender):
 	add_button.title=("0 oz today")
 	
 w, h = ui.get_screen_size()
+x_middle = w/2
+
 view = ui.View(frame = (0,0,w,h))                            
 view.name = 'Drink Tracker'                             
 view.background_color = 'white'  
 
-
-x_middle = w/2
-
-
 add_button = ui.Button(title='add water', flex = 'LRTB', font=('<System>',24))
-add_button.frame = (0,0,150,110)
+add_button.frame = (0,0,w,h/2)
 add_button.action = button_tapped                
-add_button.center = (x_middle, h/2.5) 
+add_button.center = (x_middle, h/2.25) 
 
-goal_label = ui.Label(frame = (125, 30,125, 140), font = ('<System>', 16), alignment = ui.ALIGN_CENTER)
+goal_label = ui.Label(frame = (0, 0,w,h), font = ('<System>', 16), alignment = ui.ALIGN_CENTER)
 goal_label.text = "1 gal = 128 oz"
+goal_label.center=(x_middle,h/8)
 
 reset_button=ui.Button(title='reset', font = ('<System>', 16))
 reset_button.center = (x_middle, h/1.5)
-#reset_button.frame = (0,50,150,160)
 reset_button.action=reset_tap
 
 view.add_subview(reset_button)
